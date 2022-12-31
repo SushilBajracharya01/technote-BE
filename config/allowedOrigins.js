4a9ebf369d0955e6f1ddeb7cf92d097f3b9bf9d1
SushilBajracharya01 <bajracharya.sushil01@gmail.com>
2022-12-31 15:59:55 +0545

    fix: update in origin

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,6 +1,6 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'https://technotes-sushil.onrender.com/',
+    'https://technotes-sushil.onrender.com',
 ];
 
 module.exports = allowedOrigins;
